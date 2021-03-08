47eae66b2cee4382035d825c771485735c3f3231
cairdcoinheringaahing <valyrio.email@gmail.com>
2021-03-08 18:07:51 +0000

    improved it, fixed loops

--- bwfuckery.py
+++ bwfuckery.py
@@ -1,102 +1,106 @@
 import sys
 
-if __name__ != '__main__':
-	raise ImportError('Not allowed to import this fuckery')
+def main(argv, stdin):
+	first_tape = [0]
+	second_tape = [0]
+	code = argv[1]
+	tapes = "-t" in argv[2:]
+	debug = "-d" in argv[2:]
+	numeric = "-n" in argv[2:]
+	tape_head = 0
+	code_index = 0
+	loop_depth = 0
+	input_index = 0
+	
+	while code_index < len(code):
+		char = code[code_index]
+		if char == "^":
+			first_tape[tape_head] ^= second_tape[tape_head]
+		if char == "&":
+			first_tape[tape_head] &= second_tape[tape_head]
+		if char == "|":
+			first_tape[tape_head] |= second_tape[tape_head]
+		if char == "~":
+			first_tape[tape_head] = ~first_tape[tape_head]
+		if char == "@":
+			first_tape, second_tape = second_tape, first_tape
+		if char == "%":
+			first_tape[tape_head], second_tape[tape_head] = second_tape[tape_head], first_tape[tape_head]
+		if char == '{':
+			first_tape[tape_head] <<= second_tape[tape_head]
+		if char == '}':
+			first_tape[tape_head] >>= second_tape[tape_head]
 
-first_tape = [0]
-second_tape = [0]
-code = sys.argv[1]
-tapes = "-t" in sys.argv[2:]
-debug = "-d" in sys.argv[2:]
-numeric = "-n" in sys.argv[2:]
-stdin = sys.stdin.read()
-tape_head = 0
-code_index = 0
-loop_depth = 0
-input_index = 0
+		if char == "+":
+			first_tape[tape_head] += 1
+		if char == "-":
+			first_tape[tape_head] -= 1
+		if char == ">":
+			tape_head += 1
+			try: first_tape[tape_head]
+			except:
+				first_tape.append(0)
+				second_tape.append(0)
+		if char == "<":
+			tape_head -= 1
+			if tape_head < 0:
+				tape_head %= len(first_tape)
+		if char == ".":
+			if numeric: func = str
+			else: func = chr
+			print(end=func(abs(int(first_tape[tape_head]))))
+		if char == ",":
+			first_tape[tape_head] = ord(stdin[input_index]) if input_index < len(stdin) else -1
+			input_index += 1
 
-def nth_index(string, substring, N, reverse):
-	if string.count(substring) < N:
-		return -1
-	string = string[::reverse]
-	found = 0
-	for i in range(len(string)):
-		char = string[i]
-		if substring == char:
-			found += 1
-		if found == N:
-			if reverse < 0:
-				return len(string) - i
+			
+			
+		if char == "[":
+			loop_depth += 1
+			if not first_tape[tape_head]:
+				search = 1
+				while search:
+					code_index += 1
+					if code[code_index] == '[': search += 1
+					if code[code_index] == ']': search -= 1
+				
+		if char == "]":
+			if first_tape[tape_head]:
+				search = 1
+				while search:
+					code_index -= 1
+					if code[code_index] == ']': search += 1
+					if code[code_index] == '[': search -= 1
 			else:
-				return i
-	return -1
+				loop_depth -= 1
+				
+		if char == "(":
+			loop_depth += 1
+			if (not first_tape[tape_head]&1):
+				search = 1
+				while search:
+					code_index += 1
+					if code[code_index] == '(': search += 1
+					if code[code_index] == ')': search -= 1
+		if char == ")":
+			if (first_tape[tape_head]&1):
+				search = 1
+				while search:
+					code_index -= 1
+					if code[code_index] == ')': search += 1
+					if code[code_index] == '(': search -= 1
+			else:
+				loop_depth -= 1
+		code_index += 1
 
-while code_index < len(code):
-	char = code[code_index]
-	if char == "^":
-		first_tape[tape_head] ^= second_tape[tape_head]
-	if char == "&":
-		first_tape[tape_head] &= second_tape[tape_head]
-	if char == "|":
-		first_tape[tape_head] |= second_tape[tape_head]
-	if char == "~":
-		first_tape[tape_head] = ~first_tape[tape_head]
-	if char == "@":
-		first_tape, second_tape = second_tape, first_tape
-	if char == "%":
-		first_tape[tape_head], second_tape[tape_head] = second_tape[tape_head], first_tape[tape_head]
-	if char == '{':
-		first_tape[tape_head] <<= second_tape[tape_head]
-	if char == '}':
-		first_tape[tape_head] >>= second_tape[tape_head]
+		if debug:
+			print(char, code_index, tape_head, first_tape, second_tape, first_tape[tape_head]&1, file = sys.stderr)
 
-	if char == "+":
-		first_tape[tape_head] += 1
-	if char == "-":
-		first_tape[tape_head] -= 1
-	if char == ">":
-		tape_head += 1
-		try: first_tape[tape_head]
-		except:
-			first_tape.append(0)
-			second_tape.append(0)
-	if char == "<":
-		tape_head -= 1
-		if tape_head < 0:
-			tape_head %= len(first_tape)
-	if char == ".":
-		if numeric: func = str
-		else: func = chr
-		print(end=func(abs(int(first_tape[tape_head]))))
-	if char == ",":
-		first_tape[tape_head] = ord(stdin[input_index]) if input_index < len(stdin) else -1
-		input_index += 1
-	if char == "[":
-		loop_depth += 1
-		if not first_tape[tape_head]:
-			code_index = nth_index(code, "]", loop_depth, -1)
-	if char == "]":
-		if first_tape[tape_head]:
-			code_index = nth_index(code, "[", loop_depth, 1)
-		else:
-			loop_depth -= 1
-	if char == "(":
-		loop_depth += 1
-		if (not first_tape[tape_head]&1):
-			code_index = nth_index(code, "]", loop_depth, -1)
-	if char == ")":
-		if (first_tape[tape_head]&1):
-			code_index = nth_index(code, "[", loop_depth, 1)
-		else:
-			loop_depth -= 1
-	code_index += 1
+	if tapes:
+		while first_tape and first_tape[-1] == 0: first_tape.pop()
+		while second_tape and second_tape[-1] == 0: second_tape.pop()
+		print(first_tape, file = sys.stderr)
+		print(second_tape, file = sys.stderr)
 
-	if debug:
-		print(char, code_index, tape_head, first_tape, second_tape, first_tape[tape_head]&1)
-
-if tapes:
-	if '.' in code: print()
-	while first_tape and first_tape[-1] == 0: first_tape.pop()
-	while second_tape and second_tape[-1] == 0: second_tape.pop()
-	print(first_tape)
-	print(second_tape)
+if __name__ == '__main__':
+	main(sys.argv, sys.stdin.read())
